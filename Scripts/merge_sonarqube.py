81eb1ce4d6e8f77136f6d2c59161a688f37adfc4
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2025-02-20 16:29:01 +0200

    add: sonarqube metrics

---
+++ Scripts/merge_sonarqube.py
@@ -0,0 +1,52 @@
+import os
+
+import requests
+import pandas as pd
+
+USER_TOKEN = "squ_716bdb66681839207f45848b2884859ab60540d5"
+
+METRICS = {"sqale_rating": "Sqale rating",
+           "reliability_remediation_effort": "Reliability remediation effort",
+           "security_remediation_effort": "Security remediation effort",
+           "software_quality_maintainability_remediation_effort": "SQ maintainability remediation effort",
+           "software_quality_reliability_remediation_effort": "SQ reliability remediation effort",
+           "software_quality_security_remediation_effort": "SQ security remediation effort",
+           "bugs": "Bugs",
+           "code_smells": "Code smells",
+           "cognitive_complexity": "Cognitive complexity",
+           "violations": "Violations",
+           "effort_to_reach_software_quality_maintainability_rating_a": "Effort to reach rating A",
+           "duplicated_blocks": "Duplicated blocks",
+           "reliability_rating": "Reliability rating",
+           "security_hotspots": "Security hotspots",
+           "security_rating": "Security rating",
+           "critical_violations": "Critical violations",
+           "info_violations": "Info violations",
+           "major_violations": "Major violations",
+           "minor_violations": "Minor violations"}
+
+COMPONENTS = {}
+for folder in os.listdir("Projects"):
+    response = requests.get("http://localhost:9000/api/measures/component_tree", params={
+        "component": folder,
+        "metricKeys": ",".join(METRICS.keys()),
+        "ps": 500,
+        "qualifiers": "DIR"
+    }, auth=(USER_TOKEN, ''))
+    r = response.json()
+    COMPONENTS[r["baseComponent"]["name"]] = r["components"]
+
+DATA = []
+for ms_system, components in COMPONENTS.items():
+    for component in components:
+        if "src/main/java" not in component["path"]:
+            continue
+        package = component["path"].split("src/main/java/")[1].replace("/", ".")
+        datapoint = {"MS_system": ms_system, "Package": package}
+        for metric in component["measures"]:
+            datapoint[METRICS[metric["metric"]]] = metric["value"]
+        DATA.append(datapoint)
+df = pd.DataFrame(DATA)
+df = df.sort_values(by="MS_system")
+df.to_csv("Metrics/metrics_sonarqube.csv", header=True, index=False)
+
---
+++ Scripts/metrics_sonarqube.py
@@ -0,0 +1,38 @@
+import os
+import subprocess
+
+SONAR_PATH = "/Users/abakhtin22/Documents/sonar-scanner-6.2.1.4610-macosx-x64/bin/sonar-scanner"
+TOKEN = "sqa_bde2972ca3539cda86a01800002351995d20368e"
+
+
+def run_sonarqube(project_path):
+    project_name = os.path.basename(project_path)
+    print(f"Inspecting {project_name}")
+
+    # Create Understand project
+    fakedir = os.path.join(os.getcwd(), "raw_data", "fakedir")
+    os.makedirs(fakedir, exist_ok=True)
+    try:
+        print(f"\t Analyzing Project...")
+        subprocess.run([SONAR_PATH, f"-Dsonar.projectKey={project_name}", f"-Dsonar.token={TOKEN}",
+                        f"-Dsonar.sources={project_path}", "-Dsonar.language=java",
+                        "-Dsonar.nodejs.executable=/opt/homebrew/bin/node",
+                        "-Dsonar.host.url=http://localhost:9000", f"-Dsonar.java.binaries={fakedir!s}"], check=True)
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error while processing {project_name}: {e}")
+
+
+def main(master_folder):
+
+    project_folders = [f.path for f in os.scandir(master_folder) if f.is_dir()]
+    for i, project in enumerate(project_folders, start=1):
+        print(f"Progress: {i}/{len(project_folders)}")
+        run_sonarqube(project)
+        print("----------- \n")
+
+
+if __name__ == "__main__":
+    master_folder = os.path.join(os.getcwd(), "Projects")
+
+    main(master_folder)
