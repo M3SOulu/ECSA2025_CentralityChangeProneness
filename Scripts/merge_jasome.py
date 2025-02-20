76c336dbb93cdd88ada29c1cd19e304de00f3610
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2025-02-20 14:33:22 +0200

    add: jasome metrics

---
+++ Scripts/merge_jasome.py
@@ -0,0 +1,56 @@
+import os
+import xml.etree.ElementTree as ET
+
+import pandas as pd
+
+DATA_PACKAGE = []
+DATA_CLASS = []
+DATA_METHOD = []
+for folder in os.scandir(os.path.join(os.getcwd(), "raw_data", "jasome")):
+    for f in os.scandir(folder):
+        tree = ET.parse(f)
+        root = tree.getroot()
+        for child_r in root:
+            if child_r.tag == "Packages":
+                for package in child_r:
+                    package_name = package.attrib["name"]
+                    for child_p in package:
+                        if child_p.tag == "Metrics":
+                            data_point = {"MS_system": folder.name,
+                                          "Package": package_name}
+                            for metric in child_p:
+                                data_point[metric.attrib["name"]] = float(metric.attrib["value"].replace(",", "."))
+                            DATA_PACKAGE.append(data_point)
+                        elif child_p.tag == "Classes":
+                            for clas in child_p:
+                                class_name = clas.attrib["name"]
+                                for child_c in clas:
+                                    if child_c.tag == "Metrics":
+                                        data_point = {"MS_system": folder.name,
+                                                      "Package": package_name,
+                                                      "Class": class_name}
+                                        for metric in child_c:
+                                            data_point[metric.attrib["name"]] = float(metric.attrib["value"].replace(",", "."))
+                                        DATA_CLASS.append(data_point)
+                                    elif child_c.tag == "Methods":
+                                        for method in child_c:
+                                            method_name = method.attrib["name"]
+                                            for child_m in method:
+                                                if child_m.tag == "Metrics":
+                                                    data_point = {"MS_system": folder.name,
+                                                                  "Package": package_name,
+                                                                  "Class": class_name,
+                                                                  "Method": method_name}
+                                                    for metric in child_m:
+                                                        data_point[metric.attrib["name"]] = float(metric.attrib["value"].replace(",", "."))
+                                                    DATA_METHOD.append(data_point)
+
+df = pd.DataFrame(DATA_PACKAGE)
+df = df.sort_values(by="MS_system")
+df.to_csv("Metrics/metrics_jasome_package.csv", index=False, header=True)
+df = pd.DataFrame(DATA_CLASS)
+df = df.sort_values(by="MS_system")
+df.to_csv("Metrics/metrics_jasome_class.csv", index=False, header=True)
+df = pd.DataFrame(DATA_METHOD)
+df = df.sort_values(by="MS_system")
+df.to_csv("Metrics/metrics_jasome_method.csv", index=False, header=True)
---
+++ Scripts/metrics_jasome.py
@@ -0,0 +1,40 @@
+import os
+import subprocess
+
+JASOME_PATH = "/Users/abakhtin22/Documents/jasome-0.6.8-alpha/bin/jasome"
+
+
+def run_jasome(project_path, output_dir):
+    project_name = os.path.basename(project_path)
+    print(f"Inspecting {project_name}")
+
+    try:
+        print(f"\t Analyzing Project...")
+        subprocess.run([JASOME_PATH, project_path, "--output", output_dir], check=True)
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error while processing {project_name}: {e}")
+
+
+def main(master_folder):
+
+    project_folders = [f.path for f in os.scandir(master_folder) if f.is_dir()]
+    analyze_folder = []
+    output_files = []
+    for project in project_folders:
+        for root, dirs, files in os.walk(project):
+            if "src" in dirs:
+                analyze_folder.append(os.path.join(project, root, "src"))
+                output_files.append(os.path.join(os.getcwd(), "raw_data", "jasome", os.path.basename(project), f"{os.path.basename(root)}.xml"))
+
+    for i, (project, output) in enumerate(zip(analyze_folder, output_files), start=1):
+        print(f"Progress: {i}/{len(analyze_folder)}")
+        os.makedirs(os.path.dirname(output), exist_ok=True)
+        run_jasome(project, output)
+        print("----------- \n")
+
+
+if __name__ == "__main__":
+    master_folder = os.path.join(os.getcwd(), "Projects")
+
+    main(master_folder)
