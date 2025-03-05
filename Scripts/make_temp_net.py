d7b6bea9e76fa7af2ca25aee320f0e71eaf81c50
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2025-03-05 16:14:22 +0200

    fix: only store necessary services add: store weight for tenetan

--- Scripts/make_temp_net.py
+++ Scripts/make_temp_net.py
@@ -6,14 +6,16 @@ import networkx as nx
 
 v = ["v0.0.1", "v0.0.2", "v0.0.3", "v0.0.4", "v0.1.0", "v0.2.0", "v1.0.0"]
 
-to_csv = [["source", "target", "version"]]
+to_csv = [["source", "target", "version", "weight"]]
 for version in v:
     with open(os.path.join("raw_data", "graph", f"train-ticket-{version}_gwcc_noDB.json"), 'r') as f:
         d = json.load(f)
     G = nx.node_link_graph(d, edges="edges", nodes="nodes", name="name", source="sender", target="receiver",
                            multigraph=False, directed=True)  # Load the graph
     for source, target in G.edges():
-        to_csv.append([source, target, version])
+        if not source.startswith("ts") or not target.startswith("ts"):
+            continue
+        to_csv.append([source, target, version, 1])
 
 with open(os.path.join("raw_data", "train-ticket-temporal.csv"), 'w') as f:
     writer = csv.writer(f, delimiter=',')
