4b50da952314d3ee9a2f7abbf3df1c0e23acb4a0
Alexander Bakhtin <alexander.bakhtin@oulu.fi>
2025-03-25 10:55:37 +0200

    fix: convert RQ2 results to odt

--- Scripts/Analysis/RQ2.py
+++ Scripts/Analysis/RQ2.py
@@ -1,7 +1,7 @@
 from collections import Counter
 import pandas as pd
 
-CCP = pd.read_excel("Results/RQ2/Wilcoxon.xlsx")
+CCP = pd.read_excel("Results/RQ2/Wilcoxon.ods")
 
 rejected = {
     "size": Counter(),
@@ -16,20 +16,19 @@ with open("Metrics/metrics_type.csv", 'r') as f:
         metric, type_ = line.split(',')
         metric_types[metric] = type_
 
-CCP = CCP[CCP["Version Id"] > 1]
-software_metrics = CCP["Y"].unique()
+software_metrics = CCP["by_Variable"].unique()
 included_corr = set()
 metric_counter = Counter()
 for metric in software_metrics:
     # Get all correlations of specific centrality and metric across time
-    time_series = CCP[CCP["Y"] == metric]
+    time_series = CCP[CCP["by_Variable"] == metric]
     # Keep a metric if it is always stat. sig. correlated
     if all(time_series["pvalue"] <= 0.01):
         included_corr.add(metric)
         # Iterate over each hypothesis
     for tup in time_series.itertuples():
-        metric = tup.Y
-        version = tup[2]
+        metric = tup.by_Variable
+        version = tup.Version_Id
         metric_type = metric_types[metric]
         if tup.pvalue <= 0.01:
             metric_counter[metric] += 1
@@ -40,6 +39,5 @@ for version in range (1,8):
     print("\tSize rejected:", rejected["size"][version])
     print("\tComplexity rejected:", rejected["complexity"][version])
     print("\tQuality rejected:", rejected["quality"][version])
-print(metric_counter)
 print(metric_counter.total())
 
